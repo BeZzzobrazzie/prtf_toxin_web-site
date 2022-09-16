ed1d749158241a553065e3186c6ede75ea7ff112
BeZzzobrazzie <61871773+BeZzzobrazzie@users.noreply.github.com>
2022-09-16 19:49:27 +0300

    +

--- src/blocks/text-field-input/text-field-input.js
+++ src/blocks/text-field-input/text-field-input.js
@@ -1,13 +1,33 @@
-/*document.addEventListener('click', delTheDefaultValue);
+document.addEventListener('click', delTheDefaultValue);
+
+let elem = document.querySelector('.text-field-input');
+
+elem.addEventListener('blur', delTheDefaultValue2);
+//elem.onblur = function() {
+//  elem.value = "222";
+//}
 
 function delTheDefaultValue() {
 
-  if(event.target.dataset.test != undefined) {
-    console.log(event.target.value);
-    if(event.target.value == 'Email') {
-      event.target.value = '';
-    }
+  let i;
+
+  if(event.target.dataset.pressing == "False") {
+    //event.target.value = '';
+    //event.target.dataset.pressing = "True";
+    //event.target.value = "111";
+    //i = event.target;
+    //console.log(event.target.value);
   }
+
+
 }
 
-*/
+function delTheDefaultValue2() {
+  console.log('1');
+  let elem = document.querySelector('.text-field-input');
+  console.log(elem.value);
+  if(elem.value == '') {
+    elem.value = '10000';
+    console.log('2');
+  }
+}
