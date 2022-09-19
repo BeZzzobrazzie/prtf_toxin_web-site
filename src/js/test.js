cfdf59bc03493cf9acd7e9682ca00074b990c8e5
BeZzzobrazzie <61871773+BeZzzobrazzie@users.noreply.github.com>
2022-09-19 19:50:14 +0300

    +

---
+++ src/js/test.js
@@ -0,0 +1,23 @@
+class calendar {
+  constructor() {
+    let arr = document.querySelectorAll('.text-field-input');
+    for (let elem of arr) {
+      elem.addEventListener('focus', this.handleTextFieldInputFocus);
+      elem.addEventListener('blur', this.handleTextFieldInputBlur);
+    }
+  }
+
+  handleTextFieldInputFocus() {
+    if(event.target.value == event.target.getAttribute('value')) {
+      event.target.value = '';
+    }
+  }
+  
+  handleTextFieldInputBlur() {
+    if(event.target.value == '') {
+      event.target.value = event.target.getAttribute('value');
+    }
+  }
+}
+
+let objCalendar = new calendar();
