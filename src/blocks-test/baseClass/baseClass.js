62014a0c68a1e7f9bfaaff19300a13b0bf999bdf
BeZzzobrazzie <n-klim29@mail.ru>
2022-10-03 18:44:09 +0300

    +

--- src/blocks-test/baseClass/baseClass.js
+++ src/blocks-test/baseClass/baseClass.js
@@ -12,7 +12,9 @@ export class baseClass {
 
       for(let c of child.classList) {
         //сделаем допущение, что на одном DOM-элементе может быть только один класс (что не верно, ибо это не так)
-
+        if(this.checkClassBem(c) == 'block' || this.checkClassBem(c) == 'mod') {
+          console.log('test');
+        }
         let prom = import('../' + this.pathToModule(c) + '.js');
         prom.then(
           result => this.handlerImportResult(result), 
@@ -37,6 +39,24 @@ export class baseClass {
   handlerImportError(error, child) {
     console.log(error);
     let i = this.createBlank(child);
+  }
+  checkClassBem(cls) {
+    if(!cls.includes('_')) {
+      console.log('it is block');
+      return 'block'
+    }
+    else if(cls.includes(cls.match(/[^_]__[^_]/)) && !cls.includes(cls.match(/[^_]_[^_]/))) {
+      console.log('it is element');
+      return 'element'
+    }
+    else if(cls.includes(cls.match(/[^_]_[^_]/))) {
+      console.log('it is mod');
+      return 'mod'
+    }
+    else {
+      console.log('error');
+      return 'error'
+    }
   }
 
   /*
